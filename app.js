e5613d232c2867e467f5eef7c46e8f0087b4b17b
Hafsasohail18 <shafsa187@gmail.com>
2020-08-09 17:43:19 +0500

    eleventh assignment

---
+++ app.js
@@ -0,0 +1,86 @@
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+}
+
+Quiz.prototype.getQuestionIndex = function() {
+    return this.questions[this.questionIndex];
+}
+
+Quiz.prototype.guess = function(answer) {
+    if(this.getQuestionIndex().isCorrectAnswer(answer)) {
+        this.score++;
+    }
+
+    this.questionIndex++;
+}
+
+Quiz.prototype.isEnded = function() {
+    return this.questionIndex === this.questions.length;
+}
+
+
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+
+Question.prototype.isCorrectAnswer = function(choice) {
+    return this.answer === choice;
+}
+
+
+function populate() {
+    if(quiz.isEnded()) {
+        showScores();
+    }
+    else {
+        var element = document.getElementById("question");
+        element.innerHTML = quiz.getQuestionIndex().text;
+
+        var choices = quiz.getQuestionIndex().choices;
+        for(var i = 0; i < choices.length; i++) {
+            var element = document.getElementById("choice" + i);
+            element.innerHTML = choices[i];
+            guess("btn" + i, choices[i]);
+        }
+
+        showProgress();
+    }
+};
+
+function guess(id, guess) {
+    var button = document.getElementById(id);
+    button.onclick = function() {
+        quiz.guess(guess);
+        populate();
+    }
+};
+
+
+function showProgress() {
+    var currentQuestionNumber = quiz.questionIndex + 1;
+    var element = document.getElementById("progress");
+    element.innerHTML = "Question " + currentQuestionNumber + " of " + quiz.questions.length;
+};
+
+function showScores() {
+    var gameOverHTML = "<h1>Result</h1>";
+    gameOverHTML += "<h2 id='score'> Your scores: " + quiz.score + "</h2>";
+    var element = document.getElementById("quiz");
+    element.innerHTML = gameOverHTML;
+};
+
+var questions = [
+    new Question("What does Au stand for in the periodic table?", ["Au", "He","Pb", "Fe"], "Au"),
+    new Question("What is the largest country in the world?", ["Turkey", "Germany", "Russia", "China"], "Russia"),
+    new Question("When Pakistan came into being?", ["1945", "1946","1947", "1948"], "1947"),
+    new Question("How many teeth does an adult human have?", ["30", "32", "33", "35"], "32"),
+    new Question("How many days does it take for the Earth to orbit the Sun?", ["368", "367", "366", "365"], "365")
+];
+
+var quiz = new Quiz(questions);
+
+populate();
